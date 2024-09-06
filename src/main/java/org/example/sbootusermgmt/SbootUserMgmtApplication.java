0c6361ad40128c4098438ba45e7da5c9279ca4b9
GreggtheDev <greggthedev@gmail.com>
2024-09-05 20:34:09 -0400

    Initial commit

---
+++ src/main/java/org/example/sbootusermgmt/SbootUserMgmtApplication.java
@@ -0,0 +1,13 @@
+package org.example.sbootusermgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootUserMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootUserMgmtApplication.class, args);
+	}
+
+}
